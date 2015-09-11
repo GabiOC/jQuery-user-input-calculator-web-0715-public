684a1cceb3991ebca14dcf942b3d300a3c7362ce
GabiOC <oconnor.gabriele@gmail.com>
2015-09-11 09:41:32 -0400

    all done

--- js/calc.js
+++ js/calc.js
@@ -1,8 +1,25 @@
-'use-strict';
-$(function(){
-
-  var calculator = new Calculator();
-
-});
-
 // your code here!
+
+var Calculator = function(){
+
+  $("#equals").click(validate);
+};
+
+function validate(){
+  var $num1 = $("#number1").val();
+  var $num2 = $("#number2").val();
+  var $operator = $("#operation").val();
+
+  var mathOperators = ['+', '-', '*', '/'];
+
+  if (!($num1.match(/^\d+$/) && $num2.match(/^\d+$/))) {
+    $("#result").text('Sorry, one of those is not a valid number!');
+  }
+  else if (mathOperators.indexOf($operator) == -1) {
+    $("#result").text('Sorry, not a valid operation!');
+  }
+  else {
+    var total = eval(parseInt($num1) + $operator + parseInt($num2));
+    $("#result").text(total);
+  }
+};
